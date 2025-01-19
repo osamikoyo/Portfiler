b329b0a1729b5d3edc79936800e6aafe8b67822c
osamikoyo <v.pylayev@mail.ru>
2025-01-19 13:55:37 +0500

    add service

--- internal/data/portfile.go
+++ internal/data/portfile.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"time"
 
 	"github.com/osamikoyo/portfiler/internal/data/models"
 	"go.mongodb.org/mongo-driver/bson"
@@ -13,23 +14,37 @@ type PortfolioStorage struct{
 	ctx context.Context
 }
 
+func New() (*PortfolioStorage, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	defer cancel()
+
+	conn, err := mongo.Connect(ctx)
+	defer conn.Disconnect(ctx)
+
+	collection := conn.Database("portfiles").Collection("portfiles")
+	return &PortfolioStorage{
+		Collection: collection,
+		ctx: ctx,
+	}, err
+}
+
 func (storage *PortfolioStorage) Add(portfolio models.Portfolio) error {
 	_, err := storage.Collection.InsertOne(storage.ctx, portfolio)
 	return err
 }
 
-func (storage *PortfolioStorage) Get(title string) (models.Portfolio, error) {
+func (storage *PortfolioStorage) Get(title string) ([]models.Portfolio, error) {
 	filter, err := storage.Collection.Find(storage.ctx, bson.M{
 		"title" : title,
 	})
 	if err != nil{
-		return models.Portfolio{}, err
+		return nil, err
 	}
 
-	var portfolias models.Portfolio
+	var portfolias []models.Portfolio
 
 	if err := filter.All(storage.ctx, &portfolias);err != nil{
-		return models.Portfolio{}, err
+		return nil, err
 	}
 
 	return portfolias, nil
--- internal/service/portfile.go
+++ internal/service/portfile.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"github.com/osamikoyo/portfiler/internal/data"
+	"github.com/osamikoyo/portfiler/internal/data/models"
+)
+
+type PortfileService struct{
+	Data *data.PortfolioStorage
+}
+
+func (service *PortfileService) Add(portfile models.Portfolio) error {
+	return service.Data.Add(portfile)
+}
+
+func (service *PortfileService) Get(title string) ([]models.Portfolio, error) {
+	return service.Data.Get(title)
+}
+
+func (service *PortfileService) AddReview(title string, review models.Review) error {
+	return service.Data.AddReview(title, review)
+}
